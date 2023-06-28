f62601e02629bec18a2e4397104e7d99d3171222
JelgerE <38977386+JelgerE@users.noreply.github.com>
2023-06-28 16:40:39 +0200

    Install kaltoa-package for positioning

---
+++ Install kaltoa-package.R
@@ -0,0 +1,5 @@
+
+# Get latest version of rstan
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# Install Kaltoa
+devtools::install_git(url = "https://gitlab.com/RTbecard/kaltoa.git", build_manual = T)
