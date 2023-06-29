3a73c93bf03fd1fe586ae5905f6ac0c769dd33b5
JelgerE <38977386+JelgerE@users.noreply.github.com>
2023-06-29 10:04:11 +0200

    Get receiver positions from trimble

--- Average trimble positions.R
+++ Average trimble positions.R
@@ -1,5 +1,7 @@
 source("Libraries.R")
 
+recs <- read.csv("./data/Receivers_IDs_locaties_deployment_2023.csv") 
+
 trimble <- read.csv("./data/All_trimble_data_receiver_positions.csv") %>%
   mutate(Name = gsub(pattern="REC-", replacement="", x=Name)) %>%
   filter(!grepl( "GPS-DRIFTER-A-BASISSTATION", Name)) 
@@ -12,14 +14,23 @@ trimble <- cbind(trimble,
                  ) 
 
 trimble <- trimble %>% 
-  select(-Name) %>%
-  rename(Name = V1,
-         Measure = V2)
-
-GPS <- trimble %>%
+  dplyr::select(-Name) %>%
+  dplyr::rename(Name = V1,
+         Measure = V2) %>%
   group_by(Name) %>%
   summarise(Easting = mean(Easting),
             Northing = mean(Northing)) %>%
   st_as_sf(coords = c("Easting", "Northing"), crs="EPSG:31370", remove = F) %>%
   st_transform(crs = "EPSG:4326")
 
+trimble <- trimble %>%
+  dplyr::mutate(deploy_long = st_coordinates(trimble)[,1],
+                deploy_lat = st_coordinates(trimble)[,2])
+
+for (rec in unique(recs$Code)) {
+  recs[(recs$Code == rec),]$Deploy_long <- trimble[(trimble$Name == rec),]$deploy_long[[1]]
+  recs[(recs$Code == rec),]$Deploy_lat <- trimble[(trimble$Name == rec),]$deploy_lat
+}
+
+#write.csv(recs, "./data/Receivers_IDs_locaties_deployment_2023.csv",
+#          row.names=F)
