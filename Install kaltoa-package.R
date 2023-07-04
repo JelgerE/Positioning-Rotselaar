dc6f89baf1cfe6f305adfc64c2f61ff0435f209f
JelgerE <38977386+JelgerE@users.noreply.github.com>
2023-07-04 16:05:17 +0200

    Install kaltoa-package

--- Install kaltoa-package.R
+++ Install kaltoa-package.R
@@ -2,4 +2,4 @@
 # Get latest version of rstan
 install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 # Install Kaltoa
-devtools::install_git(url = "https://gitlab.com/RTbecard/kaltoa.git", build_manual = T)
+devtools::install_git(url = "https://gitlab.com/RTbecard/kaltoa.git", build_manual = T, force = T)
